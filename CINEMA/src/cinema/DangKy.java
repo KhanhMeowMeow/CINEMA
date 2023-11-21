01bca4f4658ec28be5c6705442ba0bd25aab86ea
KhanhMeowMeow <136141884+KhanhMeowMeow@users.noreply.github.com>
2023-11-21 13:43:49 +0700

    cập nhật

--- CINEMA/src/cinema/DangKy.java
+++ CINEMA/src/cinema/DangKy.java
@@ -55,7 +55,7 @@ public class DangKy {
     }
     
     private void myGUI(){
-        System.out.println("Tay đổi nhỏ");
+        System.out.println("Sửa");
        mainFrame = new JFrame();
        mainFrame.setSize(w, h);
        mainFrame.setLayout(null);
