09ecbca66f129526f40733b9f7a2973c925e0799
khanh <nguyenkhanhahihivl@gmail.com>
2023-11-21 13:45:55 +0700

    thay đổi

--- CINEMA/src/cinema/DangKy.java
+++ CINEMA/src/cinema/DangKy.java
@@ -55,7 +55,7 @@ public class DangKy {
     }
     
     private void myGUI(){
-        System.out.println("Sửa");
+        System.out.println("khánh");
        mainFrame = new JFrame();
        mainFrame.setSize(w, h);
        mainFrame.setLayout(null);
