e81f8587def96ee68a277a77342ea0912812b875
KhanhMeowMeow <136141884+KhanhMeowMeow@users.noreply.github.com>
2023-11-23 17:33:43 +0700

    update

--- src/cinema/QuanLyVe.java
+++ src/cinema/QuanLyVe.java
@@ -39,7 +39,7 @@ public class QuanLyVe extends JFrame{
           setDefaultCloseOperation(EXIT_ON_CLOSE);
           SpringLayout layout_container = new SpringLayout();
           setLayout(layout_container);
-          
+          System.out.println("Hahahaha");
           
           //Thêm components
           panPhim = new JPanel();
